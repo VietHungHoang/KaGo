fb6d940de8ca5b035371e72c1da8528ffc605f57
VietHungHoang <viethunghoang1508@gmail.com>
2025-06-13 15:06:10 +0700

    chore: Center application window on startup

--- src/app.py
+++ src/app.py
@@ -6,8 +6,22 @@ class Application(tk.Tk):
         super().__init__()
 
         self.title(APP_NAME)
-        self.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
+        
+        self.center_window()
 
         # In a message to confirm it's running
-        label = tk.Label(self, text="KaGo đã sẵn sàng!", font=("Arial", 18))
+        label = tk.Label(self, text="Cửa sổ chính của KaGo đã sẵn sàng!", font=("Arial", 18))
         label.pack(pady=100)
+
+    """Caculate and set position to make the window appear centered on the screen"""
+    def center_window(self):
+        # Get screen dimensions
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+
+        # Calculate x, y coordinates
+        x_coordinate = int((screen_width / 2) - (WINDOW_WIDTH / 2))
+        y_coordinate = int((screen_height / 2) - (WINDOW_HEIGHT / 2))
+
+        # Set the window geometry with the new position
+        self.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{x_coordinate}+{y_coordinate}")
